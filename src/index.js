9becedbdeae4e1779a465c8d191a641afcf01210
ayoadeoye1 <isadeoye02@gmail.com>
2023-01-30 19:04:14 +0100

    bug fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import {  BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
